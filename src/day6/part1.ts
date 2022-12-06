759015ae3af3ff9c2b8f5dc12d9a50c27810a21e
StefGeraets <stef.geraets@gmail.com>
2022-12-06 09:02:48 +0100

    Use Set instead of Map to find unique array slice

--- src/day6/part1.ts
+++ src/day6/part1.ts
@@ -4,16 +4,9 @@ const datastream = getInput().split("");
 const AMOUNT_TO_MATCH = 4
 
 const startOfPacker = datastream.map((marker, i) => {
-  const current = datastream.slice(i, i+AMOUNT_TO_MATCH);
-  const uniqueMarkers = new Map();
+  const uniqueMarkers = new Set(datastream.slice(i, i+AMOUNT_TO_MATCH));
 
-  return current.some(item => {
-    if(uniqueMarkers.has(item)) {
-      return false;
-    }
-    uniqueMarkers.set(item, {});
-    return uniqueMarkers.size === AMOUNT_TO_MATCH ? true : false
-  });
+  if(uniqueMarkers.size === AMOUNT_TO_MATCH) return true;
 })
 
 const getFirstMatch = startOfPacker.findIndex(item => item === true) + AMOUNT_TO_MATCH;
